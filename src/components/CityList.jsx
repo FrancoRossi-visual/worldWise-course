a6f219aae9a1558e77fd1ec5cf3ff1b911923db7
Fran <francorossi.visual@gmail.com>
2023-10-29 11:27:07 -0300

    cities and countries

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable react/prop-types */
+import CityItem from './CityItem';
 import styles from './CityList.module.css';
+import Spinner from './Spinner.jsx';
+import Message from './Message.jsx';
 
-function CityList() {
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message='Add your fist city by clicking on a city on the map' />
+    );
   return (
     <ul className={styles.cityList}>
-      <li>console.lo</li>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
     </ul>
   );
 }
+
 export default CityList;
