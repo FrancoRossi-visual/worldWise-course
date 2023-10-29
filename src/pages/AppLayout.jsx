bd6dedbd1e129d1abbeafce282e8ca480a5cedb0
Fran <francorossi.visual@gmail.com>
2023-10-28 22:36:13 -0300

    finished components and nested navigation, started on the app page

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,12 @@
+import Map from '../components/Map';
 import SideBar from '../components/SideBar';
+import styles from './AppLayout.module.css';
 
 function AppLayout() {
   return (
-    <div>
+    <div className={styles.app}>
       <SideBar />
+      <Map />
     </div>
   );
 }
