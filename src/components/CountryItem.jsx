d15e6c1b4eff1fc4ff3c17ea2f01834bcf0577dd
Fran <francorossi.visual@gmail.com>
2023-10-29 18:51:46 -0300

    state in url, search params to map and city object

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,10 +1,11 @@
-import styles from "./CountryItem.module.css";
+import styles from './CountryItem.module.css';
 
 function CountryItem({ country }) {
+  const { emoji } = country;
   return (
     <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
-      <span>{country.country}</span>
+      <span>{emoji}</span>
+      <span>name</span>
     </li>
   );
 }
